053906a9cee37a9688a9950c27101da041216ae4
Britanskii <terraforbritain@gmail.com>
2022-07-18 21:10:19 +0300

    later fix it

--- store/index.ts
+++ store/index.ts
@@ -2,6 +2,7 @@ import {configureStore} from "@reduxjs/toolkit";
 import dictionaryReducer from "./reducers/dictionaryReducer";
 
 export const store = configureStore({
+    // @ts-ignore
     reducer: dictionaryReducer
 })
 
